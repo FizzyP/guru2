fa4a2e39846ad634f8281fcd047afce141257c24
FizzyP <fabrizio.polo@signifyd.com>
2015-10-28 18:14:38 -0600

    Nothing much

--- src/com/fabriziopolo/guru/Main.java
+++ src/com/fabriziopolo/guru/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        args = new String[]{"done"};
+//        args = new String[]{"done"};
 
         //  Open "guru.txt" and try to parse it
         try {
